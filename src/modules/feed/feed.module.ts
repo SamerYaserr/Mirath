27e84b85655f1780f4fabdc8833964b93868b665
Raghad Islam <raghad.noser.2003@gmail.com>
2026-01-17 09:16:29 +0200

    chore(feed): remove redundant PrismaService #39

--- src/modules/feed/feed.module.ts
+++ src/modules/feed/feed.module.ts
@@ -3,11 +3,10 @@ import { FeedController } from './feed.controller';
 import { FeedService } from './feed.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { FeedRepository } from './repositories/feed.repository';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [FeedController],
-  providers: [FeedService, FeedRepository, PrismaService],
+  providers: [FeedService, FeedRepository],
 })
 export class FeedModule {}
