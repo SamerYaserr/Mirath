ba30be50d33389649fd7ad1efc071418f5550b96
Sohila <sohila.haddad@gmail.com>
2026-04-13 23:34:51 +0200

    feat(paper-annotations): implement edit notes functionality

--- src/modules/paper-annotations/paper-annotations.controller.ts
+++ src/modules/paper-annotations/paper-annotations.controller.ts
@@ -1,18 +1,19 @@
 import {
-  Body,
-  Controller,
-  HttpCode,
-  HttpStatus,
-  Param,
-  Post,
   Req,
+  Body,
+  Post,
+  Patch,
+  Param,
+  HttpCode,
+  Controller,
+  HttpStatus,
 } from '@nestjs/common';
 import type { Request } from 'express';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { HttpResponse } from 'src/common/types/api.types';
 import { HighlightParamsDto } from './dto/highlight-params.dto';
-import { TakeNoteReqDto } from './dto/requests/take-note.req.dto';
+import { TakeNoteReqDto } from './dto/requests/note.req.dto';
 import { PaperAnnotationsService } from './paper-annotations.service';
 
 @ApiTags('Paper Annotations')
@@ -32,5 +33,16 @@ export class PaperAnnotationsController {
   ): Promise<HttpResponse> {
     const userId = req.user!.id;
     return this.paperAnnotationsService.takeNote(userId, highlightId, id, note);
+  }
+
+  @Patch('/:highlightId/note')
+  @HttpCode(HttpStatus.OK)
+  editNote(
+    @Req() req: Request,
+    @Body() { note }: TakeNoteReqDto,
+    @Param() { highlightId, id }: HighlightParamsDto,
+  ): Promise<HttpResponse> {
+    const userId = req.user!.id;
+    return this.paperAnnotationsService.editNote(userId, highlightId, id, note);
   }
 }
--- src/modules/paper-annotations/paper-annotations.service.ts
+++ src/modules/paper-annotations/paper-annotations.service.ts
@@ -30,11 +30,46 @@ export class PaperAnnotationsService {
 
     if (highlight.userId !== userId)
       throw new ForbiddenException(
-        'You do not have permission to do this action.',
+        'You do not have permission to perform this action.',
       );
     if (highlight.note)
       return new ConflictException(
         'This highlight already has a note. Use PATCH to update it.',
+      );
+
+    const updatedHighlight = await this.highlightsRepository.setNote(
+      highlightId,
+      note,
+    );
+
+    return {
+      data: HighlightResDto.fromEntity(updatedHighlight),
+    };
+  }
+
+  async editNote(
+    userId: string,
+    highlightId: string,
+    paperId: string,
+    note: string,
+  ): Promise<HttpResponse> {
+    const highlight = await this.highlightsRepository.find(highlightId);
+    if (!highlight)
+      throw new NotFoundException('No highlight found with this id.');
+
+    if (highlight.paperId !== paperId)
+      throw new BadRequestException(
+        'This highlight does not belong to the given paper.',
+      );
+
+    if (highlight.userId !== userId)
+      throw new ForbiddenException(
+        'You do not have permission to perform this action.',
+      );
+
+    if (!highlight.note)
+      return new NotFoundException(
+        'No note found on this highlight. Use POST to create one.',
       );
 
     const updatedHighlight = await this.highlightsRepository.setNote(
