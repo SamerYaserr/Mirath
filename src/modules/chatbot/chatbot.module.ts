b02857a234bb865e8c9aa08bd2ccb9c78cde5865
Raghad Islam <raghad.noser.2003@gmail.com>
2026-05-07 22:34:12 +0300

    feat(chatbot): integrate Cloudinary module and ChatImagePipe for image support

--- src/modules/chatbot/chatbot.module.ts
+++ src/modules/chatbot/chatbot.module.ts
@@ -5,10 +5,17 @@ import ChatbotService from './chatbot.service';
 import ChatbotController from './chatbot.controller';
 import ChatSessionsRepository from './repositories/sessions.repository';
 import ChatMessagesRepository from './repositories/messages.repository';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { ChatImagePipe } from '../../common/pipes/chat-image.pipe';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CloudinaryModule],
   controllers: [ChatbotController],
-  providers: [ChatSessionsRepository, ChatMessagesRepository, ChatbotService],
+  providers: [
+    ChatSessionsRepository,
+    ChatMessagesRepository,
+    ChatbotService,
+    ChatImagePipe,
+  ],
 })
 export default class ChatbotModule {}
