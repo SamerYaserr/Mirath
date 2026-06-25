414ea58ecf1a9419bf8dff2af19009c5ffb0966d
SamerYaserr <samersmora1290@gmail.com>
2026-06-25 19:32:11 +0300

    feat(users): add daily cron job to purge expired user accounts #143
    - Add UsersCronService with @Cron(EVERY_DAY_AT_MIDNIGHT) to
      permanently delete users past their scheduledDeletionAt date
    - Add deleteExpiredAccounts() to UsersRepository using raw SQL
      DELETE ... RETURNING to leverage Prisma cascade deletes
    - Register UsersCronService as a provider in UsersModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,11 +5,17 @@ import { InterestsModule } from '../interests/interests.module';
 import { UsersController } from './users.controller';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 import { FollowsRepository } from './repositories/follows.repository';
+import { UsersCronService } from './users.cleanup.service';
 
 @Module({
   imports: [InterestsModule, CloudinaryModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, FollowsRepository],
+  providers: [
+    UsersService,
+    UsersRepository,
+    FollowsRepository,
+    UsersCronService,
+  ],
   exports: [UsersRepository, UsersService, FollowsRepository],
 })
 export class UsersModule {}
