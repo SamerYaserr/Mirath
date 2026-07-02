ae80648dab21d69277a0243c42e12149cad355f7
Sohila <sohila.haddad@gmail.com>
2026-07-02 18:11:45 +0300

    feat(users): emit follow notification

--- src/modules/notifications/notification.processor.ts
+++ src/modules/notifications/notification.processor.ts
@@ -69,8 +69,8 @@ export class NotificationProcessor extends WorkerHost {
 
     await this.sendPush(
       targetUserId,
+      `New Follower`,
       `${actorName} started following you`,
-      '',
       {
         type: NotificationType.FOLLOW,
         actorUserId,
